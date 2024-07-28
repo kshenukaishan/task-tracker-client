58109f549e6301ee068d7dccbcec43afdbf74f7b
kshenukaishan <kshenukaishan@outlook.com>
2024-07-28 22:54:04 +0530

    implemented routes for admin and employee modules etc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { AdminModule } from './modules/admin/admin.module';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./auth/components/login/login.component').then(
+        (cmp) => cmp.LoginComponent
+      ),
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./auth/components/signup/signup.component').then(
+        (cmp) => cmp.SignupComponent
+      ),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./modules/admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'employee',
+    loadChildren: () =>
+      import('./modules/employee/employee.module').then(
+        (m) => m.EmployeeModule
+      ),
+  },
+];
