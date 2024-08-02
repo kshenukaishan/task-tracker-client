2c7897df0d3a298109335c0c05276399b2bfcf91
kshenukaishan <kshenukaishan@outlook.com>
2024-08-02 22:10:56 +0530

    created admin employee dashboard logout functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { AdminModule } from './modules/admin/admin.module';
-import { EmployeeModule } from './modules/employee/employee.module';
+import { StorageService } from './auth/services/storage/storage.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    MatToolbarModule,
-    RouterLink,
-    AdminModule,
-    EmployeeModule,
-  ],
+  imports: [RouterOutlet, MatToolbarModule, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'task-tracker-client';
+export class AppComponent implements OnInit {
+  isEmployeeLoggedIn: boolean = StorageService.isUserLogged();
+  isAdminLoggedIn: boolean = StorageService.isAdminLogged();
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      this.isEmployeeLoggedIn = StorageService.isUserLogged();
+      this.isAdminLoggedIn = StorageService.isAdminLogged();
+    });
+  }
+
+  logout() {
+    StorageService.logout();
+    this.router.navigate(['/login']);
+  }
 }
