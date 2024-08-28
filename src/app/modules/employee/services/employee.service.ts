edfb6656c037c6e50fc407aaf9b983a86ae405df
kshenukaishan <kshenukaishan@outlook.com>
2024-08-28 11:22:53 +0530

    added getTask method in employee service

--- src/app/modules/employee/services/employee.service.ts
+++ src/app/modules/employee/services/employee.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { StorageService } from '../../../auth/services/storage/storage.service';
+import { Observable } from 'rxjs';
+
+const BASE_URL = 'http://localhost:8080/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getEmployeeTasksById(): Observable<any> {
+    return this.http.get(BASE_URL + 'api/v1/employee/tasks', {
+      headers: this.createAuthorizationHeader(),
+    });
+  }
+
+  private createAuthorizationHeader(): HttpHeaders {
+    return new HttpHeaders().set(
+      'Authorization',
+      'Bearer ' + StorageService.getTtoken()
+    );
+  }
 }
